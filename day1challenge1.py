b3ffdc1eb3fc1ee33ba64cff4ac8d5031aab7b0b
Adam Millican <adamdm1@gmail.com>
2021-05-09 16:44:35 +0100

    Day 1 Challenge 1

---
+++ day1challenge1.py
@@ -0,0 +1,9 @@
+def calculateFuel(mass):
+    return (mass // 3) - 2
+
+
+with open("day1input.txt", "r") as input_file:
+    count = 0
+    for line in input_file.readlines():
+        count += calculateFuel(int(line))
+    print(count)
